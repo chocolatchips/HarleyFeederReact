70867ea13c6b24dc3e36ef828ca6e9bc3f771198
CHRISLAPTOP\choco <chocolat_chips1@yahoo.ca>
2024-08-13 19:51:22 -0600

    Started preliminary work for requests

--- harleyfeeder/src/App.js
+++ harleyfeeder/src/App.js
@@ -1,22 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+
 
 function App() {
+  const [data, setData] = useState(null);
+  const [test, setTest] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const date = new Date(Date.now()).toLocaleTimeString().slice(16,18);
+  
+  useEffect(() => {
+    setLoading(true);
+    fetch(`https://www.sheard.ca/harley/${date > 12 ? 'evening': 'morning'}`, {method: "GET"   })
+    .then((response) => response.json())
+    .then(setData)
+    .catch(setError);
+  }, []);
+
+  useEffect(() => {
+    setLoading(true);
+    fetch('https://www.sheard.ca/harley')
+    .then((response) => response.json())
+    .then(setTest)
+    .then(() => setLoading(false))
+    .catch(setError);
+  }, []);
+
+  if(loading)
+      return (<h1>Loading...</h1>);
+  if (error)
+    return (<pre>{JSON.stringify(error)}</pre>);
+  if(!data) return null;
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+        <h1>{data.isFed ? "Fed" : "Not Fed"}</h1>
+        <h2>{data.type}</h2>
+        <h2>{data.date}</h2>
+        <h2>{test.date}</h2>
+        <h2>{test.result}</h2>
+        <h2>{test.isFed ? 'true' : 'false'}</h2>
       </header>
     </div>
   );
